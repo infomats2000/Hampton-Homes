a884431705f0cbf43376812652e7651c1e8018ea
Nasir Mahmood <infomats.net@gmail.com>
2026-08-22 22:54:53 +1000

    Include PublicNavbar header bar on landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import React from "react";
+import PublicLayout from "./(public)/layout";
 import HomePage from "./(public)/page";
 
-export default HomePage;
+export default function RootHomePage() {
+  return (
+    <PublicLayout>
+      <HomePage />
+    </PublicLayout>
+  );
+}
